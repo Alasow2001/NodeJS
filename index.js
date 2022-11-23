313761165d8741a97afa79ec859c1e95e5849b5e
Ahmed Alasow <a.alasow01@gmail.com>
2022-11-23 13:25:34 +0000

    Added content for the quetions array (title, description, license, usage).

--- index.js
+++ index.js
@@ -1,13 +1,70 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your README document?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Type in a description of your project: ',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Type in steps on how to install your project: ',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Type in how to use your project: '
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Enter links that assisted you in creating the project: ',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license: ',
+        choices: [
+            {
+                name: 'None',
+                value: 'None',
+            },
+            {
+                name: 'MIT License',
+                value: 'MITLicense',
+            },
+        ],
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username: ',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address: ',
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(filename, data, (err) => err ? console.log(err): console.log('Successful README generated'));
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+}
 
 // Function call to initialize app
 init();
